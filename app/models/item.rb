dd25ab9ab53027d89b23ff5c7b5a9906458e2c8a
Onthevillage <104823266+Onthevillage@users.noreply.github.com>
2022-06-01 20:33:39 +0900

    バリデーション・アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+    validates :image, presence: true
+    validates :name, presence: true
+    validates :info, presence: true
+    validates :category_id, presence: true
+    validates :sales_status_id, presence: true
+    validates :shipping_fee_status_id, presence: true
+    validates :prefecture_id, presence: true
+    validates :scheduled_delivery_id, presence: true
+    validates :price, presence: true, numericality: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
+    validates :user, presence: true
+
+    #has_one :order
+    belongs_to :user
 end
