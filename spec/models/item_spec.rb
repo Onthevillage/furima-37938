1db37020b88d152ce68bf3a39f08a06ff0c8e260
Onthevillage <104823266+Onthevillage@users.noreply.github.com>
2022-06-02 22:27:04 +0900

    itemモデル単体テストコード実装途中①

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,7 +8,34 @@ RSpec.describe Item, type: :model do
   describe '商品出品機能' do
     context '商品出品できる場合' do
       it 'バリデーションに引っ掛からなければ登録できる' do
-        @item..valid?
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できない場合' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'infoが空では登録できない' do
+        @item.info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+      it 'category_idが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'category_idが1では登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
     end
   end
