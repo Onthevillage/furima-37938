6a0ada886459d4a3d302d63b5770b444c5cbeae4
Onthevillage <104823266+Onthevillage@users.noreply.github.com>
2022-06-05 14:06:48 +0900

    レビュー修正依頼対応①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_params_id, only: [:show, :edit, :update]
-  before_action :only_to_item_user, only: [:edit, :update]
+  before_action :only_to_item_user, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
