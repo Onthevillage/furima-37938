0ff36e7e8bc67194776dc52c5175d27b010b439e
Onthevillage <104823266+Onthevillage@users.noreply.github.com>
2022-06-09 18:49:30 +0900

    レビュー修正依頼対応⑥
    「with_options presence: true do」への変更

--- app/models/order_pay_form.rb
+++ app/models/order_pay_form.rb
@@ -2,15 +2,16 @@ class OrderPayForm
     include ActiveModel::Model
     attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :token
 
-    validates :user_id, presence: true
-    validates :item_id, presence: true
-    validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
-    validates :city, presence: true
-    validates :addresses, presence: true
-    validates :phone_number, presence: true, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
-    validates :token, presence: true
-
+    with_options presence: true do
+        validates :user_id
+        validates :item_id
+        validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+        validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+        validates :city
+        validates :addresses
+        validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
+        validates :token
+    end
 
     def save
         order = Order.create(user_id: user_id, item_id: item_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birth_date
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
